75add184382a4386ed41aa2fdbfefdc43e38f4d0
George_Lewis <georgejlewis@hotmail.com>
2018-07-19 12:29:21 +0200

    add web pack bundle shortcut and index file

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+
+  },
+  plugins: [
+  new HtmlWebpackPlugin()
+  ]
+};

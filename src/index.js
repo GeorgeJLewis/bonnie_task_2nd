7fa07239e8aad77a4626e2a974465426942dc679
George_Lewis <georgejlewis@hotmail.com>
2018-07-20 14:42:57 +0200

    add js for api had to remove append child as it was displaying nil so needs investigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import api from './api';
+document.addEventListener('DOMContentLoaded', () => {
+  const displayElement = document.getElementById('listings')
 
-console.log(api);
+  const displayListings = data => {
+      const listings = data.newsPlayer.assets
+      listings.forEach(listing => {
+          let el = document.createElement('div')
+          el.dataset.id = listing.id
+          el.innerHTML = `<h1>${listing.program_nid}</h1><p>${listing.description}</p>`
+          // displayElement.appendChild(el)
+      });
+  }
+
+fetch('https://tv4-graphql-prod.herokuapp.com/graphql?query={newsPlayer%20{assets%20{%20id%20program_nid%20description}}}')
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (json) {
+      displayListings(json.data)
+  });
+})
